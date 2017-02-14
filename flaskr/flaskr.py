62b23d5e7c8562e982322d4f866014fa2d255603
vikram47 <vikramjeet.singh@quovantis.com>
2017-02-14 11:45:57 +0530

    Initial Setup Done

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -10,12 +10,55 @@ app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='secret'
 ))
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
+# Routes
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('SELECT title, text FROM entries ORDER BY id DESC')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('INSERT INTO entries (title, text) VALUES (?, ?)', [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+# DB related functions
 def connect_db():
     """Connects to the specific database."""
     print('Inside connect_db.')
@@ -52,3 +95,7 @@ def close_db(error):
     print('Inside close_db.')
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+
+if __name__ == '__main__':
+    app.run()
